f88b583976c4aef0462cb471b794a7e720533784
Tomas Sandnes <tomas.sandnes@commodore.no>
2021-07-03 12:24:40 +0200

    Rollback to: "Commit f716c6e8 | Added more Tutorial text".

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -7,7 +7,10 @@ namespace ConsoleApp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello fantastic summer class of C# 2021!");
-            Console.WriteLine("This is a kind of live ttorial for GitHub.");
+            Console.WriteLine("This is a kind of live tutorial for GitHub.");
+            Console.WriteLine();
+            Console.WriteLine("To undo changes, there's a couple options:");
+            Console.WriteLine("1. Git -> History -> Revert | This takes you back 1 version (do it n times to get to older versions.)");
         }
     }
 }
