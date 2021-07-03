f716c6e89c9dd0b1936f9011f1521f7dac47a30a
Tomas Sandnes <tomas.sandnes@commodore.no>
2021-07-03 12:15:51 +0200

    Added more tutorial text.

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -8,6 +8,9 @@ namespace ConsoleApp
         {
             Console.WriteLine("Hello fantastic summer class of C# 2021!");
             Console.WriteLine("This is a kind of live tutorial for GitHub.");
+            Console.WriteLine();
+            Console.WriteLine("To undo changes, there's a couple options:");
+            Console.WriteLine("1. Git -> History -> Revert | This takes you back 1 version (do it n times to get to older versions.)");
         }
     }
 }
