15566210071aa7c8bb14f48179ffe432d592e4bb
Tomas Sandnes <tomas.sandnes@commodore.no>
2021-07-03 12:34:16 +0200

    Completed the tutorial-text about revert and rollback.

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -10,7 +10,18 @@ namespace ConsoleApp
             Console.WriteLine("This is a kind of live tutorial for GitHub.");
             Console.WriteLine();
             Console.WriteLine("To undo changes, there's a couple options:");
-            Console.WriteLine("1. Git -> History -> Revert | This takes you back 1 version (do it n times to get to older versions.)");
+            Console.WriteLine();
+            Console.WriteLine("1. This takes you back 1 version: (do it n times to get to older versions)");
+            Console.WriteLine("   ------------------------------");
+            Console.WriteLine("   Git -> History -> Revert");
+            Console.WriteLine();
+            Console.WriteLine("2. This works as a rollback to a previous version:");
+            Console.WriteLine("   -----------------------------------------------");
+            Console.WriteLine("   Git -> History -> Open wanted version.");
+            Console.WriteLine("   Ctrl+A, Ctrl+C (copy all contents).");
+            Console.WriteLine("   Open current version of file, then: Ctrl+A, Ctrl+V (overwrite current with the contents from clipboard).");
+            Console.WriteLine("   Commit with comment about what version you did a rollback to.");
+            Console.WriteLine();
         }
     }
 }
